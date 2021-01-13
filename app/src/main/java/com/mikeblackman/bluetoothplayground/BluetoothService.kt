b02c0a9de8be4b04c03333e7fc4189dbf8d0d2ad
mikeblackman <mblackman@dynamiccontrols.com>
2021-01-13 15:29:43 +1300

    Refactor BluetoothService

--- app/src/main/java/com/mikeblackman/bluetoothplayground/BluetoothService.kt
+++ app/src/main/java/com/mikeblackman/bluetoothplayground/BluetoothService.kt
@@ -1,6 +1,5 @@
 package com.mikeblackman.bluetoothplayground
 
-import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import kotlinx.coroutines.*
@@ -10,24 +9,50 @@ import java.io.OutputStream
 import java.util.*
 import javax.inject.Inject
 
+/**
+ * Helper function for printing the contents of a byteArray to the command line
+ * Useful for debugging
+ */
+fun ByteArray.printByteArray(): String? {
+    val sb = StringBuilder()
+    sb.append("[ ")
+    for (b in this) {
+        sb.append(String.format("0x%02X ", b))
+    }
+    sb.append("]")
+    return sb.toString()
+}
+
+/**
+ * Extension function that connects to a device asynchronously
+ * and returns a connected socket.
+ */
+private suspend fun BluetoothDevice.connectAsClientAsync(uuid: UUID) =
+    supervisorScope {
+        coroutineScope {
+            return@coroutineScope async(IO) {
+                val bluetoothSocket = createRfcommSocketToServiceRecord(uuid)
+                bluetoothSocket.also { it.connect() }
+            }
+        }
+    }
 
 interface BluetoothService {
     suspend fun connect(device: BluetoothDevice): Boolean
-    suspend fun sendPacketForResponse(packet : ByteArray) : ByteArray
-    suspend fun writeNoResponse(packet : ByteArray)
-    fun isConnected() : Boolean
+    suspend fun sendPacketForResponse(packet: ByteArray): ByteArray
+    suspend fun sendPacketNoResponse(packet: ByteArray)
+    fun isConnected(): Boolean
     fun disconnect()
 }
 
 class BluetoothServiceImpl @Inject constructor(
-    private val bluetoothAdapter: BluetoothAdapter,
-    ) : BluetoothService {
+) : BluetoothService {
 
     companion object {
         val GUID_LAK: UUID = UUID.fromString("ABB03DDB-B693-4844-BC79-0E5A54058248")
     }
 
-    private var job = Job()
+    private var connectionJob = Job()
     private var bluetoothSocket: BluetoothSocket? = null
     private var device: BluetoothDevice? = null
     private var inputStream: InputStream? = null
@@ -35,51 +60,27 @@ class BluetoothServiceImpl @Inject constructor(
 
     /// Example of a single thread context for co-routine
     // val context = newSingleThreadContext("BluetoothContext")
-    /**
-     * Extension function that connects to a device asynchronously
-     * and returns a connected socket.
-     */
-    private suspend fun BluetoothDevice.connectAsClientAsync(uuid: UUID) =
-        supervisorScope {
-            coroutineScope {
-                return@coroutineScope async(IO) {
-                    val bluetoothSocket = createRfcommSocketToServiceRecord(uuid)
-                    bluetoothSocket.also { it.connect() }
-                }
-            }
-        }
 
     override suspend fun connect(device: BluetoothDevice): Boolean {
-
-        job = Job()
-
+        connectionJob = Job()
         this.device = device
 
-        val state = CoroutineScope(IO + job).async(SupervisorJob(job)) {
-
+        val state = CoroutineScope(IO + connectionJob).async(SupervisorJob(connectionJob)) {
             bluetoothSocket = device.connectAsClientAsync(GUID_LAK).await()
             inputStream = bluetoothSocket!!.inputStream
             outputStream = bluetoothSocket!!.outputStream
-
-            // val initialConnectionPacket = bluetoothPacketProvider.encodePacket(0x01, toolVersionPayload())
-            // I explicitly want to block here as I don't want to read until this has finished
-//             outputStream?.write(byteArrayOf())
-//
-//            val result = supervisorScope { async { read(inputStream!!) } }
-//            result.await().toTypedArray().toByteArray()
-
             bluetoothSocket?.isConnected ?: false
         }
         return state.await()
     }
 
-    override suspend fun sendPacketForResponse(packet : ByteArray) : ByteArray {
+    override suspend fun sendPacketForResponse(packet: ByteArray): ByteArray {
         outputStream?.write(packet)
         val resultBytes = supervisorScope { async { read(inputStream!!) } }
         return resultBytes.await().toTypedArray().toByteArray()
     }
 
-    override suspend fun writeNoResponse(packet : ByteArray) {
+    override suspend fun sendPacketNoResponse(packet: ByteArray) {
         outputStream?.write(packet)
     }
 
@@ -88,17 +89,20 @@ class BluetoothServiceImpl @Inject constructor(
     }
 
     override fun disconnect() {
-        if (isConnected()){
+        if (isConnected()) {
             inputStream?.close()
             outputStream?.close()
             bluetoothSocket?.close()
-            job.cancel()
+            connectionJob.cancel()
             device = null
         }
     }
 
     private fun read(inputStream: InputStream): MutableList<Byte> {
-
+        /**
+         * This function assumes data is delimited by 0xF0 bytes
+         * This is will be different depending on protocol
+         */
         val result = mutableListOf<Byte>()
         var readFirstByte = false
         var readLastByte = false
@@ -114,18 +118,4 @@ class BluetoothServiceImpl @Inject constructor(
         }
         return result
     }
-}
-
-/**
- * Helper function for printing the contents of a byteArray to command line
- * Useful for debugging
- */
-fun ByteArray.printByteArray(): String? {
-    val sb = StringBuilder()
-    sb.append("[ ")
-    for (b in this) {
-        sb.append(String.format("0x%02X ", b))
-    }
-    sb.append("]")
-    return sb.toString()
 }
